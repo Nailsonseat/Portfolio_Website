a4d3a7f8707f1de3fb1bb3faa72df744ec0dac93
nailsonseat <aadarshv3@gmail.com>
2023-06-23 18:44:14 +0530

    Extracted a separate launch page under its animation provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_website/pages/launch_page.dart';
+import 'package:portfolio_website/providers/launch_page_animation_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,18 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(body: Center(child: Text('Portfolio',textScaleFactor: 5,))),
+      home: Scaffold(
+        body: ChangeNotifierProvider(
+          child: const LaunchPage(),
+          create: (_) => LaunchPageAnimationProvider(),
+        ),
+      ),
     );
   }
 }
