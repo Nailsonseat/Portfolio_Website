bcdcdba69a5589bed66cc168be536fea334cb90d
nailsonseat <aadarshv3@gmail.com>
2023-06-24 11:59:56 +0530

    Set the url strategy to path

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:portfolio_website/pages/launch_page.dart';
 import 'package:portfolio_website/providers/launch_page_animation_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
+  setUrlStrategy(PathUrlStrategy());
 }
 
 class MyApp extends StatelessWidget {
