17049ae0ba1d76313433417cb5138c1d75d2d121
nailsonseat <aadarshv3@gmail.com>
2023-06-24 13:49:50 +0530

    Added launch page.

--- lib/pages/launch_page.dart
+++ lib/pages/launch_page.dart
@@ -15,128 +15,132 @@ class LaunchPage extends StatelessWidget {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       Provider.of<LaunchPageProvider>(context, listen: false).startAnimation();
     });
-    return Row(
-      children: [
-        Expanded(
-          flex: 2,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.grey[200],
+    return Scaffold(
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.grey[200],
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(color: Colors.grey[200]),
+                        ),
+                        Expanded(
+                          flex: 4,
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Row(
+                              children: [
+                                AutoSizeText(
+                                  'Hello! My name is Aadarsh',
+                                  style: TextStyle(fontSize: width / 33, fontFamily: 'Quicksand'),
+                                  minFontSize: 15,
+                                ),
+                                AnimatedTextKit(
+                                  repeatForever: true,
+                                  animatedTexts: [
+                                    TyperAnimatedText(
+                                      "...",
+                                      textStyle: TextStyle(fontSize: width / 33, fontFamily: 'Quicksand'),
+                                      speed: const Duration(milliseconds: 500),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
                   child: Row(
                     children: [
                       Expanded(
-                        flex: 2,
+                        flex: 1,
                         child: Container(color: Colors.grey[200]),
                       ),
                       Expanded(
-                        flex: 4,
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Row(
-                            children: [
-                              AutoSizeText(
-                                'Hello! My name is Aadarsh',
-                                style: TextStyle(fontSize: width / 33, fontFamily: 'Quicksand'),
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: Container(
+                                color: Colors.grey[200],
+                                child: Row(
+                                  children: [
+                                    AutoSizeText('I do ',
+                                        style: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand'),minFontSize: 20,),
+                                    AnimatedTextKit(
+                                      //onFinished: () => context.go('/home'),
+                                      repeatForever: false,
+                                      isRepeatingAnimation: false,
+                                      animatedTexts: [
+                                        RotateAnimatedText(
+                                          'Development',
+                                          textStyle: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand'),
+                                          transitionHeight: 100,
+                                        ),
+                                        RotateAnimatedText('Machine Learning',
+                                            textStyle: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand'),
+                                            transitionHeight: 100),
+                                        RotateAnimatedText('both!',
+                                            rotateOut: false,
+                                            textStyle: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand'),
+                                            transitionHeight: 100,
+                                            duration: const Duration(milliseconds: 500)),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
-                              AnimatedTextKit(
-                                repeatForever: true,
-                                animatedTexts: [
-                                  TyperAnimatedText(
-                                    "...",
-                                    textStyle: TextStyle(fontSize: width / 33, fontFamily: 'Quicksand'),
-                                    speed: const Duration(milliseconds: 500),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
+                            ),
+                            Expanded(
+                              flex: 8,
+                              child: Container(color: Colors.grey[200]),
+                            )
+                          ],
                         ),
                       ),
                     ],
                   ),
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Container(color: Colors.grey[200]),
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Container(
-                              color: Colors.grey[200],
-                              child: Row(
-                                children: [
-                                  AutoSizeText('I do ',
-                                      style: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand')),
-                                  AnimatedTextKit(
-                                    repeatForever: false,
-                                    isRepeatingAnimation: false,
-                                    animatedTexts: [
-                                      RotateAnimatedText(
-                                        'Development',
-                                        textStyle: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand'),
-                                        transitionHeight: 100,
-                                      ),
-                                      RotateAnimatedText('Machine Learning',
-                                          textStyle: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand'),
-                                          transitionHeight: 100),
-                                      RotateAnimatedText('Both!',
-                                          rotateOut: false,
-                                          textStyle: TextStyle(fontSize: width / 39.5, fontFamily: 'Quicksand'),
-                                          transitionHeight: 100,
-                                          duration: const Duration(milliseconds: 500)),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 8,
-                            child: Container(color: Colors.grey[200]),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.grey[200],
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Consumer<LaunchPageProvider>(
-                builder: (_, animationProvider, __) {
-                  return CircularPercentIndicator(
-                    addAutomaticKeepAlive: true,
-                    radius: width / 33,
-                    animation: true,
-                    animationDuration: 1000,
-                    lineWidth: 10.0,
-                    animateFromLastPercent: true,
-                    percent: animationProvider.percent,
-                    circularStrokeCap: CircularStrokeCap.butt,
-                    progressColor: Colors.grey[200],
-                  );
-                },
-              ),
+                )
+              ],
             ),
           ),
-        )
-      ],
+          Expanded(
+            child: Container(
+              color: Colors.grey[200],
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Consumer<LaunchPageProvider>(
+                  builder: (_, animationProvider, __) {
+                    return CircularPercentIndicator(
+                      addAutomaticKeepAlive: true,
+                      radius: width / 33,
+                      animation: true,
+                      animationDuration: 1000,
+                      lineWidth: width / 197.4,
+                      animateFromLastPercent: true,
+                      percent: animationProvider.percent,
+                      circularStrokeCap: CircularStrokeCap.butt,
+                      progressColor: Colors.grey[200],
+                    );
+                  },
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
