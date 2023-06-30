099690cd55273a7e07bd97a4f5f87edb3c60575b
nailsonseat <aadarshv3@gmail.com>
2023-06-30 13:29:12 +0530

    Implemented scroll to top button.

--- lib/pages/home/home_main.dart
+++ lib/pages/home/home_main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/pages/home/home_page_one.dart';
 import 'package:portfolio_website/pages/home/home_page_two.dart';
+import 'package:portfolio_website/providers/scroll_to_top_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeMain extends StatelessWidget {
-   HomeMain({super.key});
-
-  final ScrollController _scrollController = ScrollController();
+  const HomeMain({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     double width = MediaQuery.of(context).size.width;
     width = width < 800 ? 800 : width;
+
+    ScrollTopProvider scrollProvider = Provider.of<ScrollTopProvider>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
@@ -26,11 +27,29 @@ class HomeMain extends StatelessWidget {
         ),
         title: const Text('Aadarsh Verma'),
       ),
+      floatingActionButton: Consumer<ScrollTopProvider>(builder: (context, scrollProvider, _) {
+        return scrollProvider.showFloatingButton
+            ? Container(
+                height: 80,
+                width: 80,
+                margin: const EdgeInsets.only(bottom: 25, right: 25),
+                child: FloatingActionButton(
+                  onPressed: scrollProvider.scrollToTop,
+                  backgroundColor: Colors.greenAccent,
+                  child: const Icon(
+                    Icons.arrow_upward,
+                    size: 30,
+                  ),
+                ),
+              )
+            : Container();
+      }),
       body: SizedBox(
         width: width,
-        child: const SingleChildScrollView(
+        child: SingleChildScrollView(
+          controller: scrollProvider.scrollController,
           scrollDirection: Axis.vertical,
-          child: SingleChildScrollView(
+          child: const SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Column(
               children: [
