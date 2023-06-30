6b2ee08a6ed0555820ac8c705b43ec605ac47a55
nailsonseat <aadarshv3@gmail.com>
2023-06-30 14:09:05 +0530

    Configured scroll provider.

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:portfolio_website/pages/home/home_main.dart';
 import 'package:portfolio_website/pages/launch/launch_page.dart';
 import 'package:portfolio_website/providers/launch_page_animation_provider.dart';
+import 'package:portfolio_website/providers/scroll_to_top_provider.dart';
 import 'package:provider/provider.dart';
 
 final GoRouter routes = GoRouter(
@@ -15,7 +16,7 @@ final GoRouter routes = GoRouter(
         GoRoute(
           path: 'home',
           pageBuilder: (context, state) => CustomTransitionPage(
-            child: HomeMain(),
+            child: ChangeNotifierProvider(child: const HomeMain(), create: (_) => ScrollTopProvider()),
             key: state.pageKey,
             transitionDuration: const Duration(milliseconds: 1000),
             transitionsBuilder: (_, animation, ___, child) {
