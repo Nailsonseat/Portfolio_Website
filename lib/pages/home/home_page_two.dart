3dcec5614e513d6d46e8eb7368f9b20d2102a76a
nailsonseat <aadarshv3@gmail.com>
2023-06-30 13:28:19 +0530

    Fetch for about me page assets using new dart 3 functionalities.

--- lib/pages/home/home_page_two.dart
+++ lib/pages/home/home_page_two.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePageTwo extends StatelessWidget {
   const HomePageTwo({super.key});
 
+  Future<({String aboutMe, String education})> getMyInfo() async {
+    String aboutMe = await rootBundle.loadString("lib/assets/text/about_me/about_me.txt");
+    String education = await rootBundle.loadString("lib/assets/text/about_me/education.txt");
+
+    aboutMe = aboutMe.replaceAll('\n', ' ');
+
+    return (aboutMe: aboutMe, education: education);
+  }
+
   @override
   Widget build(BuildContext context) {
-
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     width = width < 800 ? 800 : width;
@@ -19,8 +26,8 @@ class HomePageTwo extends StatelessWidget {
       padding: const EdgeInsets.only(top: 50),
       color: Colors.transparent,
       child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 150,vertical: 70),
-        padding: const EdgeInsets.symmetric(horizontal: 80,vertical: 30),
+        margin: const EdgeInsets.symmetric(horizontal: 150, vertical: 70),
+        padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 30),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           color: Colors.grey[200],
@@ -37,7 +44,7 @@ class HomePageTwo extends StatelessWidget {
           children: [
             SelectableText(
               'About me',
-              style: GoogleFonts.workSans(
+              style: GoogleFonts.inter(
                 fontSize: 55,
                 //decoration: TextDecoration.underline,
                 //fontWeight: FontWeight.bold,
@@ -48,12 +55,14 @@ class HomePageTwo extends StatelessWidget {
             const SizedBox(
               height: 40,
             ),
-            FutureBuilder(
-              future: rootBundle.loadString("lib/assets/about_me.txt"),
-              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            FutureBuilder<({String aboutMe, String education})>(
+              future: getMyInfo(),
+              builder: (BuildContext context, AsyncSnapshot <({String aboutMe, String education})> snapshot) {
                 if (snapshot.hasData) {
+                      String aboutMe = snapshot.data?.aboutMe ?? '';
+                      String education = snapshot.data?.education ?? '';
                   return SelectableText(
-                    snapshot.data!,
+                    aboutMe + education,
                     style: GoogleFonts.robotoMono(fontSize: 20),
                   );
                 }
