2ce3f3964f9c5ad3965dc9b5abaccf27ba57d341
nailsonseat <aadarshv3@gmail.com>
2023-07-02 00:13:50 +0530

    Defined project slider provider in widget tree.

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:portfolio_website/pages/home/home_main.dart';
 import 'package:portfolio_website/pages/launch/launch_page.dart';
 import 'package:portfolio_website/providers/launch_page_animation_provider.dart';
+import 'package:portfolio_website/providers/project_slider_provider.dart';
 import 'package:portfolio_website/providers/scroll_to_top_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -16,7 +17,13 @@ final GoRouter routes = GoRouter(
         GoRoute(
           path: 'home',
           pageBuilder: (context, state) => CustomTransitionPage(
-            child: ChangeNotifierProvider(child: const HomeMain(), create: (_) => ScrollTopProvider()),
+            child: MultiProvider(
+              providers: [
+                ChangeNotifierProvider(create: (_) => ScrollTopProvider()),
+                ChangeNotifierProvider(create: (_) => ProjectSliderProvider()),
+              ],
+              child: const HomeMain(),
+            ),
             key: state.pageKey,
             transitionDuration: const Duration(milliseconds: 1000),
             transitionsBuilder: (_, animation, ___, child) {
