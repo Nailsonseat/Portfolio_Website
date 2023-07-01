c3f709594af87113ab435db4b77c45f39d35cffa
nailsonseat <aadarshv3@gmail.com>
2023-07-01 23:02:03 +0530

    Linked project page as third page in home section.

--- lib/pages/home/home_main.dart
+++ lib/pages/home/home_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/pages/home/home_page_one.dart';
+import 'package:portfolio_website/pages/home/home_page_three.dart';
 import 'package:portfolio_website/pages/home/home_page_two.dart';
 import 'package:portfolio_website/providers/scroll_to_top_provider.dart';
 import 'package:provider/provider.dart';
@@ -55,6 +56,7 @@ class HomeMain extends StatelessWidget {
               children: [
                 HomePageOne(),
                 HomePageTwo(),
+                HomePageThree()
               ],
             ),
           ),
