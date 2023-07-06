0fda71cc475b795e6d6ac32fcdfb24fefc6e1c0b
nailsonseat <aadarshv3@gmail.com>
2023-07-06 12:13:16 +0530

    Assignmed images to project tiles.

--- lib/providers/project_page_provider.dart
+++ lib/providers/project_page_provider.dart
@@ -25,47 +25,47 @@ class ProjectPageProvider extends ChangeNotifier {
         description: ''),
     Project(
         title: "E-commerce Shopper's Behaviour Understanding",
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/e_commerce_behaviour_understanding.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'Data Into IPL',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/data_in_ipl.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'Portfolio Website',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/portfolio.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'Kastor',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/kastor.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'LetsTalkk',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/messaging.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'Yoshop Web Scraper',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/yoshop_scraper.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'Holomorphic Bot',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/hola_bot.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'Machine Learning',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/machine_Learning.png'),
         briefDescription: '',
         description: ''),
     Project(
         title: 'Classic Snake By 2D Focused Frames per second',
-        image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
+        image: Image.asset('lib/assets/images/project_images/snake.png'),
         briefDescription: '',
         description: ''),
   ];
