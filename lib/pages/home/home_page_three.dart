a14812ee6f52d238e79b0269e6d3dd390e64f8cf
nailsonseat <aadarshv3@gmail.com>
2023-07-06 12:23:02 +0530

    Project page now goes by the intrinsic height of column, and tiles are now displayed from project page provider.

--- lib/pages/home/home_page_three.dart
+++ lib/pages/home/home_page_three.dart
@@ -15,82 +15,79 @@ class HomePageThree extends StatelessWidget {
 
     return Container(
       width: width,
-      height: height * 7.5 - 80,
-      padding: EdgeInsets.only(top: 50, bottom: 50),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 150),
-            child: Text(
-              "Projects",
-              style: TextStyle(fontSize: 60),
+      padding: const EdgeInsets.only(top: 50, bottom: 50),
+      child: IntrinsicHeight(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 150),
+              child: Text(
+                "Projects",
+                style: TextStyle(fontSize: 60),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 150),
-            child: Consumer<ProjectSliderProvider>(
-              builder: (_, sliderProvider, __) {
-                return AnimatedToggleSwitch<int>.size(
-                  height: 80,
-                  current: sliderProvider.value,
-                  borderColorBuilder: (i) => Colors.black,
-                  values: List<int>.generate(sliderProvider.options.length, (i) => i),
-                  iconOpacity: 0.75,
-                  borderRadius: BorderRadius.circular(50),
-                  indicatorSize: Size.fromWidth(300),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0, 0),
-                      blurRadius: 2.0,
-                    ),
-                  ],
-                  iconAnimationType: AnimationType.onHover,
-                  indicatorAnimationType: AnimationType.onHover,
-                  iconBuilder: (value, size) => sliderProvider.optionBuilder(value, size),
-                  borderWidth: 0.1,
-                  borderColor: Colors.transparent,
-                  colorBuilder: (i) => sliderProvider.optionColor(i),
-                  onChanged: (i) => sliderProvider.onChanged(i),
-                );
-              },
+            const SizedBox(
+              height: 50,
             ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Consumer<ProjectPageProvider>(
-            builder: (_, projectProvider, __) {
-              return IntrinsicHeight(
-                child: Container(
-                  //height: height * 1.5,
-                  decoration: BoxDecoration(color: Colors.grey[100], borderRadius: BorderRadius.circular(20)),
-                  margin: const EdgeInsets.symmetric(horizontal: 90),
-                  padding: const EdgeInsets.only(top: 100, bottom: 100),
-                  child: Stack(
-                    children: [
-                      Opacity(
-                        opacity: 1.0, // Adjust the opacity value to control the level of fading
-                        child: SizedBox.expand(
-                          child: Wrap(
-                            alignment: WrapAlignment.spaceAround,
-                            direction: Axis.horizontal,
-                            runAlignment: WrapAlignment.spaceEvenly,
-                            runSpacing: 80,
-                            children: [
-                              for (Project i in projectProvider.projects)
-                                SizedBox(
-                                  width: 500,
-                                  height: 650,
-                                  child: Container(
-                                    decoration:
-                                        BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(35)),
-                                    child: Align(
-                                      alignment: Alignment.topCenter,
+            Padding(
+              padding: const EdgeInsets.only(left: 150),
+              child: Consumer<ProjectSliderProvider>(
+                builder: (_, sliderProvider, __) {
+                  return AnimatedToggleSwitch<int>.size(
+                    height: 80,
+                    current: sliderProvider.value,
+                    borderColorBuilder: (i) => Colors.black,
+                    values: List<int>.generate(sliderProvider.options.length, (i) => i),
+                    iconOpacity: 0.75,
+                    borderRadius: BorderRadius.circular(50),
+                    indicatorSize: const Size.fromWidth(300),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0, 0),
+                        blurRadius: 2.0,
+                      ),
+                    ],
+                    iconAnimationType: AnimationType.onHover,
+                    indicatorAnimationType: AnimationType.onHover,
+                    iconBuilder: (value, size) => sliderProvider.optionBuilder(value, size),
+                    borderWidth: 0.1,
+                    borderColor: Colors.transparent,
+                    colorBuilder: (i) => sliderProvider.optionColor(i),
+                    onChanged: (i) => sliderProvider.onChanged(i),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Consumer<ProjectPageProvider>(
+              builder: (_, projectProvider, __) {
+                return IntrinsicHeight(
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.grey[100], borderRadius: BorderRadius.circular(20)),
+                    margin: const EdgeInsets.symmetric(horizontal: 90),
+                    padding: const EdgeInsets.only(top: 100, bottom: 100),
+                    child: Stack(
+                      children: [
+                        Opacity(
+                          opacity: 1.0, // Adjust the opacity value to control the level of fading
+                          child: SizedBox.expand(
+                            child: Wrap(
+                              alignment: WrapAlignment.spaceAround,
+                              direction: Axis.horizontal,
+                              runAlignment: WrapAlignment.spaceEvenly,
+                              runSpacing: 80,
+                              children: [
+                                for (Project i in projectProvider.projects)
+                                  SizedBox(
+                                    width: 500,
+                                    height: 650,
+                                    child: Container(
+                                      decoration:
+                                          BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(35)),
                                       child: Column(
                                         children: [
                                           ClipRRect(
@@ -99,6 +96,7 @@ class HomePageThree extends StatelessWidget {
                                             child: i.image,
                                           ),
                                           Align(
+                                            heightFactor: 2,
                                             alignment: Alignment.centerLeft,
                                             child: Padding(
                                               padding: const EdgeInsets.only(left: 30),
@@ -107,24 +105,28 @@ class HomePageThree extends StatelessWidget {
                                                 style: const TextStyle(fontSize: 30),
                                               ),
                                             ),
+                                          ),
+                                          Container(
+                                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                                            child: Text(i.briefDescription,style: TextStyle(fontSize: 18),),
                                           )
                                         ],
                                       ),
                                     ),
                                   ),
-                                ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      //Center(child: CircularProgressIndicator()),
-                    ],
+                        //Center(child: CircularProgressIndicator()),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
-          ),
-        ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
