ffd4add5fa0ce123cfa84e5270b19010dcbf9bef
nailsonseat <aadarshv3@gmail.com>
2023-07-07 14:51:38 +0530

    Added slider switch fade effect in development and ml project pages.

--- lib/pages/projects/developments_projects_page.dart
+++ lib/pages/projects/developments_projects_page.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_website/providers/project_page_provider.dart';
+import 'package:provider/provider.dart';
 
 class DevelopmentProjectsPage extends StatelessWidget {
   const DevelopmentProjectsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Container(
-        width: 1000,
-        height: 900,
-        padding: const EdgeInsets.all(100),
-        child: Image.asset("lib/assets/images/miscellaneous/under_construction.png"),
-      ),
+    return Consumer<ProjectPageProvider>(
+      builder: (_, projectPageProvider, __) {
+        return Opacity(
+          opacity: projectPageProvider.opacity,
+          child: SizedBox.expand(
+            child: Container(
+              width: 1000,
+              height: 900,
+              padding: const EdgeInsets.all(100),
+              child: Image.asset("lib/assets/images/miscellaneous/under_construction.png"),
+            ),
+          ),
+        );
+      },
     );
   }
 }
