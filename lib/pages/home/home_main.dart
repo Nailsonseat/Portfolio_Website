ef919417caed0031665ad7c4ac732d7862c8c422
nailsonseat <aadarshv3@gmail.com>
2023-07-10 17:58:21 +0530

    Responsive app bar and leading icon

--- lib/pages/home/home_main.dart
+++ lib/pages/home/home_main.dart
@@ -1,4 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio_website/pages/home/home_page.dart';
 import 'package:portfolio_website/pages/projects/project_browser_main.dart';
@@ -8,6 +7,11 @@ import 'package:provider/provider.dart';
 
 class HomeMain extends StatelessWidget {
   const HomeMain({super.key});
+
+  _appBarFontSize(double width, double height) {
+    double fontSize = height * width / 83065.92;
+    return fontSize < 16 ? 16 : fontSize;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,20 +32,23 @@ class HomeMain extends StatelessWidget {
           icon: const Icon(Icons.menu),
           onPressed: () {},
         ),
-        title:  AutoSizeText('Aadarsh Verma',minFontSize: 16,),
+        title: Text(
+          'Aadarsh Verma',
+          style: TextStyle(fontSize: _appBarFontSize(width, height)),
+        ),
       ),
       floatingActionButton: Consumer<ScrollTopProvider>(builder: (context, scrollProvider, _) {
         return scrollProvider.showFloatingButton
             ? Container(
-                height: 80,
-                width: 80,
+                height: width / 24.675,
+                width: width / 24.675,
                 margin: const EdgeInsets.only(bottom: 25, right: 25),
                 child: FloatingActionButton(
                   onPressed: scrollProvider.scrollToTop,
                   backgroundColor: Colors.greenAccent,
-                  child: const Icon(
+                  child: Icon(
                     Icons.arrow_upward,
-                    size: 30,
+                    size: width / 65.8,
                   ),
                 ),
               )
