cabd7dbd1ff1c418646cd36421f4482ef699406c
nailsonseat <aadarshv3@gmail.com>
2023-07-13 09:51:31 +0530

    Created a project page controller and a method to scroll to description.

--- lib/providers/scroll_provider.dart
+++ lib/providers/scroll_provider.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/cupertino.dart';
 
 class ScrollTopProvider extends ChangeNotifier {
-  final ScrollController _scrollController = ScrollController();
+  final ScrollController _homeScrollController = ScrollController();
+  final ScrollController _detailedProjectScrollController = ScrollController();
   bool _showFloatingButton = false;
 
   late double appBarHeight;
 
   ScrollTopProvider() {
-    _scrollController.addListener(_scrollListener);
+    _homeScrollController.addListener(_scrollListener);
   }
 
-  ScrollController get scrollController => _scrollController;
+  ScrollController get homeScrollController => _homeScrollController;
+  ScrollController get detailedProjectScrollController => _detailedProjectScrollController;
 
   bool get showFloatingButton => _showFloatingButton;
 
   void _scrollListener() {
-    if (_scrollController.offset >= 100) {
+    if (_homeScrollController.offset >= 100) {
       _showFloatingButton = true;
     } else {
       _showFloatingButton = false;
@@ -24,15 +26,23 @@ class ScrollTopProvider extends ChangeNotifier {
   }
 
   void scrollToTop() {
-    _scrollController.animateTo(
+    _homeScrollController.animateTo(
       0,
       duration: const Duration(milliseconds: 500),
       curve: Curves.easeInOut,
     );
   }
 
+  void scrollToProjectDescription(double position) {
+    _homeScrollController.animateTo(
+      position,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
   void scrollToProjects(double singlePageHeight) {
-    _scrollController.animateTo(
+    _homeScrollController.animateTo(
       (singlePageHeight * 2) - appBarHeight,
       duration: const Duration(milliseconds: 500),
       curve: Curves.easeInOut,
