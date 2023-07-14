fc3b0ebfcd649ea862b9eea89fc4818728c885b9
nailsonseat <aadarshv3@gmail.com>
2023-07-14 16:33:21 +0530

    Defined project provider in root.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:portfolio_website/providers/project_page_provider.dart';
 import 'package:portfolio_website/providers/scroll_provider.dart';
 import 'package:portfolio_website/routes.dart';
 import 'package:provider/provider.dart';
@@ -17,7 +18,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ScrollProvider())],
+        ChangeNotifierProvider(create: (_) => ScrollProvider()),
+        ChangeNotifierProvider(create: (_) => ProjectPageProvider()),
+      ],
       child: MaterialApp.router(
         title: 'Portfolio Website',
         theme: ThemeData(
