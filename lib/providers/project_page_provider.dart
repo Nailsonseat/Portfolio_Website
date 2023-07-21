18e907d7c79587695c3373ec3919dc5a133a62f8
nailsonseat <aadarshv3@gmail.com>
2023-07-21 12:47:56 +0530

    Updated order of projects in all projects page.

--- lib/providers/project_page_provider.dart
+++ lib/providers/project_page_provider.dart
@@ -48,6 +48,14 @@ class ProjectPageProvider extends ChangeNotifier {
             "My most ambitious project ever, This app aims to reduce a teacher's time in non teaching activities, since only 19% of a teachers time is actually sent on teaching. The app provides a huge set of features to manipulate and create questions at the click of a button.",
         description: ''),
     Project(
+        title: 'Portfolio Website',
+        image: Image.asset('lib/assets/images/project_images/portfolio.png'),
+        source: "https://github.com/Nailsonseat/Portfolio_Website",
+        pagePath: '/home/Portfolio_Website',
+        briefDescription:
+        "A website to showcase myself, my work and progress in a beautiful website, with easy to digest descriptions of each and every one of my project. ",
+        description: ''),
+    Project(
         title: 'Hospital Management Utility',
         image: Image.asset('lib/assets/images/project_images/hospital_management_utility.png'),
         source: "https://github.com/Nailsonseat/Hospital-Management-Utility",
@@ -72,20 +80,28 @@ class ProjectPageProvider extends ChangeNotifier {
             "This model predicts how much runs a team will score in the first 6 overs (also called power play). It uses data of players, their teams and the matches won by them, ever since the IPL started in 2008. ",
         description: ''),
     Project(
-        title: 'Portfolio Website',
-        image: Image.asset('lib/assets/images/project_images/portfolio.png'),
-        source: "https://github.com/Nailsonseat/Portfolio_Website",
-        pagePath: '/home/Portfolio_Website',
-        briefDescription:
-            "A website to showcase myself, my work and progress in a beautiful website, with easy to digest descriptions of each and every one of my project. ",
-        description: ''),
-    Project(
         title: 'Kastor',
         image: Image.asset('lib/assets/images/project_images/kastor.png'),
         source: "https://github.com/Nailsonseat/Kastor",
         pagePath: '/home/Kastor',
         briefDescription:
             "This is a Website that is supposed to fetch game prices from different platforms using web scraping and give you the best deal. Just like Trivago but for Games! ",
+        description: ''),
+    Project(
+        title: 'Classic Snake Focused Frames per Second',
+        image: Image.asset('lib/assets/images/project_images/snake.png'),
+        source: "https://github.com/Nailsonseat/Classic-Snake-By-2D-Focused-Frames-per-second",
+        pagePath: '/home/Classic_Snake_FFPS',
+        briefDescription:
+        "My first ever project! The classic snake game with a twist! It only updates the screen where data is updated. It records high score and saves it along with the options for snake customization as well!",
+        description: ''),
+    Project(
+        title: 'Machine Learning',
+        image: Image.asset('lib/assets/images/project_images/machine_learning.png'),
+        source: "https://github.com/Nailsonseat/Machine-Learning",
+        pagePath: '/home/Machine_Learning',
+        briefDescription:
+        "This projects has my entire machine learning journey where I hard code and implement various machine learning algorithms from scratch without using any machine learning library. ",
         description: ''),
     Project(
         title: 'LetsTalkk',
@@ -110,22 +126,6 @@ class ProjectPageProvider extends ChangeNotifier {
         pagePath: '/home/Hola_bot',
         briefDescription:
             "A tri-wheeler holomorphic bot that is able to traverse and move its limbs in all directions.",
-        description: ''),
-    Project(
-        title: 'Machine Learning',
-        image: Image.asset('lib/assets/images/project_images/machine_learning.png'),
-        source: "https://github.com/Nailsonseat/Machine-Learning",
-        pagePath: '/home/Machine_Learning',
-        briefDescription:
-            "This projects has my entire machine learning journey where I hard code and implement various machine learning algorithms from scratch without using any machine learning library. ",
-        description: ''),
-    Project(
-        title: 'Classic Snake Focused Frames per Second',
-        image: Image.asset('lib/assets/images/project_images/snake.png'),
-        source: "https://github.com/Nailsonseat/Classic-Snake-By-2D-Focused-Frames-per-second",
-        pagePath: '/home/Classic_Snake_FFPS',
-        briefDescription:
-            "My first ever project! The classic snake game with a twist! It only updates the screen where data is updated. It records high score and saves it along with the options for snake customization as well!",
         description: ''),
   ];
 }
