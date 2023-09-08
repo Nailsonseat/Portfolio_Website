78b14ab1c332a89e4bb81021fa20a14a5e6df8f7
nailsonseat <aadarshv3@gmail.com>
2023-09-08 08:22:25 +0530

    Manager rendered container heights.

---
+++ lib/providers/project_component_constraint_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProjectComponentsConstraintsProvider extends ChangeNotifier{
+  late List<GlobalKey> textKeys;
+  late List<GlobalKey> titleKeys;
+  late List<double> textContainerHeights;
+  late List<double> titleContainerHeights;
+
+  bool visible = false;
+
+  void initHeights(int len){
+    textContainerHeights = List.generate(len, (index) => 0.1);
+  }
+  void setHeight(double height,int index){
+    textContainerHeights[index]=height;
+    notifyListeners();
+  }
+}
