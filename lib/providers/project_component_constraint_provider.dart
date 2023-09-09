7968815a24b95aaa2c42e459a187764a0493e718
nailsonseat <aadarshv3@gmail.com>
2023-09-09 18:50:43 +0530

    function for text container height

--- lib/providers/project_component_constraint_provider.dart
+++ lib/providers/project_component_constraint_provider.dart
@@ -12,8 +12,12 @@ class ProjectComponentsConstraintsProvider extends ChangeNotifier {
     textContainerHeights = List.generate(len, (index) => 0.1);
     titleContainerHeights = List.generate(len, (index) => 0);
   }
-  void setHeight(double height,int index){
-    textContainerHeights[index]=height;
+
+  void setTextContainerHeight() {
+    for (int i = 0; i < textKeys.length; i++) {
+      RenderBox textContainerRenderer = textKeys[i].currentContext!.findRenderObject() as RenderBox;
+      textContainerHeights[i] = textContainerRenderer.size.height;
+    }
     notifyListeners();
   }
 }
