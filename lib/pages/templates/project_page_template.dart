215342a6e8973e5c9cb3dec3690fa6509c13efd1
nailsonseat <aadarshv3@gmail.com>
2023-09-19 09:44:08 +0530

    Decreased the minimum width constraint to 1000

--- lib/pages/templates/project_page_template.dart
+++ lib/pages/templates/project_page_template.dart
@@ -139,7 +139,7 @@ class ProjectPageTemplate extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    if (width < 1200 || height < 768) {
+    if (width < 1000 || height < 768) {
       return const SmallScreenWidget();
     }
 
