d03f3f7d76512daa642ae990127de9095424d0d6
nailsonseat <aadarshv3@gmail.com>
2023-09-29 12:57:32 +0530

    Defined the increment function

--- lib/providers/launch_page_animation_provider.dart
+++ lib/providers/launch_page_animation_provider.dart
@@ -1,8 +1,31 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 class LaunchPageProvider extends ChangeNotifier {
   double percent = 0.33;
+
+
+  Future<void> incrementVisitorCount() async {
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    DocumentReference docRef = firestore.collection('visitor_counts').doc('home_page_visitors');
+
+    try {
+      DocumentSnapshot docSnapshot = await docRef.get();
+
+      // If the document doesn't exist, create it with an initial count of 0
+      if (!docSnapshot.exists) {
+        await docRef.set({'count': 0});
+      }
+
+      // Increment the count
+      await docRef.update({'count': FieldValue.increment(1)});
+    } catch (e) {
+      Logger().w('Error incrementing visitor count: $e');
+    }
+  }
 
   void startAnimation() {
     Timer.periodic(
