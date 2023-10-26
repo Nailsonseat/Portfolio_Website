05b8a1896b04853eca8989d6b3b721b7087e587d
nailsonseat <aadarshv3@gmail.com>
2023-10-27 00:42:59 +0530

    Responsive visitor count and child dialogue pop ups

--- lib/pages/home/home_main.dart
+++ lib/pages/home/home_main.dart
@@ -50,132 +50,138 @@ class HomeMain extends StatelessWidget {
                   return ElevatedButton(
                     onPressed: () => showDialog(
                       context: context,
-                      builder: (context) => Dialog(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-                        child: IntrinsicWidth(
-                          child: IntrinsicHeight(
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(23.0),
-                                    child: Align(
-                                      alignment: Alignment.topCenter,
-                                      child: SelectableText(
-                                        "Aadarsh's Portfolio Website",
-                                        style: GoogleFonts.roboto(fontSize: 35),
+                      builder: (context) => ResponsiveScaledBox(
+                        width: 1978,
+                        child: Dialog(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+                          child: IntrinsicWidth(
+                            child: IntrinsicHeight(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20),
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(23.0),
+                                      child: Align(
+                                        alignment: Alignment.topCenter,
+                                        child: SelectableText(
+                                          "Aadarsh's Portfolio Website",
+                                          style: GoogleFonts.roboto(fontSize: 35),
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                                    child: SelectableText(
-                                      "Welcome to my portfolio website\nYou are the $count visitor\nOr you could have been the ${count - 1} visitor and just reloaded the website which would be cheating ;)",
-                                      style: const TextStyle(fontSize: 16),
-                                      textAlign: TextAlign.center,
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                                      child: SelectableText(
+                                        "Welcome to my portfolio website\nYou are the $count visitor\nOr you could have been the ${count - 1} visitor and just reloaded the website which would be cheating ;)",
+                                        style: const TextStyle(fontSize: 16),
+                                        textAlign: TextAlign.center,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    height: 40,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 23),
-                                    child: Align(
-                                      alignment: Alignment.bottomCenter,
-                                      child: Row(
-                                        crossAxisAlignment: CrossAxisAlignment.end,
-                                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                        children: [
-                                          ElevatedButton(
-                                            onPressed: () => showDialog(
-                                              context: context,
-                                              builder: (context) => Dialog(
-                                                child: Padding(
-                                                  padding: const EdgeInsets.all(0.0),
-                                                  child: FutureBuilder<String>(
-                                                    future: getLicense(),
-                                                    builder: (context, snapshot) {
-                                                      if (snapshot.hasData) {
-                                                        return Column(
-                                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                          children: [
-                                                            Expanded(
-                                                              child: SingleChildScrollView(
-                                                                child: Padding(
-                                                                  padding: const EdgeInsets.all(20.0),
-                                                                  child: Column(
-                                                                    children: [
-                                                                      SelectableText(
-                                                                        snapshot.data!,
-                                                                        textAlign: TextAlign.center,
+                                    const SizedBox(
+                                      height: 40,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(bottom: 23),
+                                      child: Align(
+                                        alignment: Alignment.bottomCenter,
+                                        child: Row(
+                                          crossAxisAlignment: CrossAxisAlignment.end,
+                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                          children: [
+                                            ElevatedButton(
+                                              onPressed: () => showDialog(
+                                                context: context,
+                                                builder: (context) => ResponsiveScaledBox(
+                                                  width: 1978,
+                                                  child: Dialog(
+                                                    child: Padding(
+                                                      padding: const EdgeInsets.all(0.0),
+                                                      child: FutureBuilder<String>(
+                                                        future: getLicense(),
+                                                        builder: (context, snapshot) {
+                                                          if (snapshot.hasData) {
+                                                            return Column(
+                                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                              children: [
+                                                                Expanded(
+                                                                  child: SingleChildScrollView(
+                                                                    child: Padding(
+                                                                      padding: const EdgeInsets.all(20.0),
+                                                                      child: Column(
+                                                                        children: [
+                                                                          SelectableText(
+                                                                            snapshot.data!,
+                                                                            textAlign: TextAlign.center,
+                                                                          ),
+                                                                        ],
                                                                       ),
-                                                                    ],
+                                                                    ),
                                                                   ),
                                                                 ),
-                                                              ),
-                                                            ),
-                                                            Padding(
-                                                              padding: const EdgeInsets.all(16.0),
-                                                              child: ElevatedButton(
-                                                                onPressed: () => context.pop(),
-                                                                style: ElevatedButton.styleFrom(
-                                                                  elevation: 2.5,
-                                                                  shape: RoundedRectangleBorder(
-                                                                    borderRadius: BorderRadius.circular(8.0),
+                                                                Padding(
+                                                                  padding: const EdgeInsets.all(16.0),
+                                                                  child: ElevatedButton(
+                                                                    onPressed: () => context.pop(),
+                                                                    style: ElevatedButton.styleFrom(
+                                                                      elevation: 2.5,
+                                                                      shape: RoundedRectangleBorder(
+                                                                        borderRadius: BorderRadius.circular(8.0),
+                                                                      ),
+                                                                      fixedSize: const Size.fromHeight(35.0),
+                                                                    ),
+                                                                    child: const Text("Ok"),
                                                                   ),
-                                                                  fixedSize: const Size.fromHeight(35.0),
                                                                 ),
-                                                                child: const Text("Ok"),
-                                                              ),
-                                                            ),
-                                                          ],
-                                                        );
+                                                              ],
+                                                            );
 
-                                                      } else {
-                                                        return const SizedBox(
-                                                            width: 100,
-                                                            height: 100,
-                                                            child: Scaffold(
-                                                              backgroundColor: Colors.transparent,
-                                                                body: Center(child: CircularProgressIndicator())));
-                                                      }
-                                                    },
+                                                          } else {
+                                                            return const SizedBox(
+                                                                width: 100,
+                                                                height: 100,
+                                                                child: Scaffold(
+                                                                  backgroundColor: Colors.transparent,
+                                                                    body: Center(child: CircularProgressIndicator())));
+                                                          }
+                                                        },
+                                                      ),
+                                                    ),
                                                   ),
                                                 ),
                                               ),
-                                            ),
-                                            style: ElevatedButton.styleFrom(
-                                              elevation: 2.5, // Add elevation if needed
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(8.0), // Adjust the radius as needed
+                                              style: ElevatedButton.styleFrom(
+                                                elevation: 2.5, // Add elevation if needed
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius: BorderRadius.circular(8.0), // Adjust the radius as needed
+                                                ),
+                                                fixedSize: const Size.fromHeight(35.0), // Adjust the height as needed
                                               ),
-                                              fixedSize: const Size.fromHeight(35.0), // Adjust the height as needed
-                                            ),
-                                            child: const Text(
-                                              "Licenses",
-                                              style: TextStyle(fontSize: 18),
-                                            ),
-                                          ),
-                                          ElevatedButton(
-                                            onPressed: () => context.pop(),
-                                            style: ElevatedButton.styleFrom(
-                                              elevation: 2.5, // Add elevation if needed
-                                              shape: RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.circular(8.0), // Adjust the radius as needed
+                                              child: const Text(
+                                                "Licenses",
+                                                style: TextStyle(fontSize: 18),
                                               ),
-                                              fixedSize: const Size.fromHeight(35.0), // Adjust the height as needed
                                             ),
-                                            child: const Text(
-                                              "Back",
-                                              style: TextStyle(fontSize: 18),
+                                            ElevatedButton(
+                                              onPressed: () => context.pop(),
+                                              style: ElevatedButton.styleFrom(
+                                                elevation: 2.5, // Add elevation if needed
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius: BorderRadius.circular(8.0), // Adjust the radius as needed
+                                                ),
+                                                fixedSize: const Size.fromHeight(35.0), // Adjust the height as needed
+                                              ),
+                                              child: const Text(
+                                                "Back",
+                                                style: TextStyle(fontSize: 18),
+                                              ),
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  )
-                                ],
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
