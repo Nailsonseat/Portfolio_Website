91bdc21860592cda068c739eca373ac64049a06d
nailsonseat <aadarshv3@gmail.com>
2024-06-27 20:34:17 +0530

    Fetch projects from a single file

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:portfolio_website/all_projects.dart';
+import 'package:portfolio_website/constants/project_constants.dart';
 import 'package:portfolio_website/pages/home/home_main.dart';
 import 'package:portfolio_website/pages/launch/launch_page.dart';
-import 'package:portfolio_website/pages/projects/foss_overflow/foss_overflow.dart';
-import 'package:portfolio_website/pages/projects/kastor/kastor.dart';
-import 'package:portfolio_website/pages/projects/letstalkk/letstalkk.dart';
-import 'package:portfolio_website/pages/projects/acharya_setu/acharya_setu.dart';
-import 'package:portfolio_website/pages/projects/classic_snake_ffps/classic_snake_ffps.dart';
-import 'package:portfolio_website/pages/projects/data_into_ipl/data_into_ipl.dart';
-import 'package:portfolio_website/pages/projects/ecommerce_shoppers_behaviour_and_understanding/ecommerce_shoppers_behaviour_and_understanding.dart';
-import 'package:portfolio_website/pages/projects/holomorphic_bot/holomorphic_bot.dart';
-import 'package:portfolio_website/pages/projects/hospital_management_utility/hospital_management_utility.dart';
-import 'package:portfolio_website/pages/projects/machine_learning/machine_learning.dart';
-import 'package:portfolio_website/pages/projects/ondc_build_for_bharat/build_for_bharat.dart';
-import 'package:portfolio_website/pages/projects/portfolio_website/portfolio_website.dart';
-import 'package:portfolio_website/pages/projects/yoshop_web_scraper/yoshop_web_scraper.dart';
 import 'package:portfolio_website/providers/launch_page_animation_provider.dart';
 import 'package:portfolio_website/providers/project_slider_provider.dart';
 import 'package:provider/provider.dart';
@@ -46,19 +35,84 @@ final GoRouter routes = GoRouter(
             },
           ),
           routes: [
-            GoRoute(path: 'Acharya_Setu', pageBuilder: (context, state) => MaterialPage(child: AcharyaSetu())),
-            GoRoute(path: 'Hospital_Management_Utility', pageBuilder: (context, state) => MaterialPage(child: HospitalManagementUtility())),
-            GoRoute(path: 'Ecommerce_Shoppers_Behaviour_and_Understanding', pageBuilder: (context, state) => MaterialPage(child: EcommerceShoppersBehaviourAndUnderstanding())),
-            GoRoute(path: 'Data_Into_IPL', pageBuilder: (context, state) => MaterialPage(child: DataIntoIPL())),
-            GoRoute(path: 'Portfolio_Website', pageBuilder: (context, state) => MaterialPage(child: PortfolioWebsite())),
-            GoRoute(path: 'Kastor', pageBuilder: (context, state) => MaterialPage(child: Kastor())),
-            GoRoute(path: 'LetsTalkk', pageBuilder: (context, state) => MaterialPage(child: LetsTalkk())),
-            GoRoute(path: 'Yoshop_Web_Scraper', pageBuilder: (context, state) => MaterialPage(child: YoshopWebScraper())),
-            GoRoute(path: 'Hola_Bot', pageBuilder: (context, state) => MaterialPage(child: HolomorphicBot())),
-            GoRoute(path: 'Machine_Learning', pageBuilder: (context, state) => MaterialPage(child: MachineLearning())),
-            GoRoute(path: 'Classic_Snake_FFPS', pageBuilder: (context, state) => MaterialPage(child: ClassicSnakeFFPS())),
-            GoRoute(path: 'Foss_Overflow', pageBuilder: (context, state) => const MaterialPage(child: FossOverflow())),
-            GoRoute(path: 'ONDC_Build_For_Bharat', pageBuilder: (context, state) => MaterialPage(child: ONDCBuildForBharat())),
+            GoRoute(
+              path: 'Acharya_Setu',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.acharyaSetu),
+              ),
+            ),
+            GoRoute(
+              path: 'Hospital_Management_Utility',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.hospitalManagementUtility),
+              ),
+            ),
+            GoRoute(
+              path: 'Ecommerce_Shoppers_Behaviour_and_Understanding',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.ecommerceShoppersBehaviourAndUnderstanding),
+              ),
+            ),
+            GoRoute(
+              path: 'Data_Into_IPL',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.dataIntoIPL),
+              ),
+            ),
+            GoRoute(
+              path: 'Portfolio_Website',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.portfolioWebsite),
+              ),
+            ),
+            GoRoute(
+              path: 'Kastor',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.kastor),
+              ),
+            ),
+            GoRoute(
+              path: 'LetsTalkk',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.letsTalkk),
+              ),
+            ),
+            GoRoute(
+              path: 'Yoshop_Web_Scraper',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.yoshopWebScraper),
+              ),
+            ),
+            GoRoute(
+              path: 'Hola_Bot',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.holomorphicBot),
+              ),
+            ),
+            GoRoute(
+              path: 'Machine_Learning',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.machineLearning),
+              ),
+            ),
+            GoRoute(
+              path: 'Classic_Snake_FFPS',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.classicSnake),
+              ),
+            ),
+            GoRoute(
+              path: 'Foss_Overflow',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.fossOverflow),
+              ),
+            ),
+            GoRoute(
+              path: 'ONDC_Build_For_Bharat',
+              pageBuilder: (context, state) => MaterialPage(
+                child: getProjectPageTemplate(ProjectNames.ondcBuildForBharat),
+              ),
+            ),
           ],
         ),
       ],
