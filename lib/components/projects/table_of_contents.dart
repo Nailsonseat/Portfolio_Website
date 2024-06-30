a0a89e6d042125765674317214d47a47d9e15dea
nailsonseat <aadarshv3@gmail.com>
2024-06-30 15:53:04 +0530

    Responsive TOC

--- lib/components/projects/table_of_contents.dart
+++ lib/components/projects/table_of_contents.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:portfolio_website/components/projects/project_component.dart';
 import 'package:provider/provider.dart';
 import '../../providers/project_component_constraint_provider.dart';
-import '../../providers/scroll_provider.dart';
-
 
 class TableOfContents extends StatelessWidget {
   const TableOfContents({super.key, required this.tableOfContents, required this.fontColor});
@@ -13,16 +12,16 @@ class TableOfContents extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    double width = 1978;
     ProjectComponentsConstraintsProvider componentsConstraintsProvider =
-    Provider.of<ProjectComponentsConstraintsProvider>(context, listen: false);
+        Provider.of<ProjectComponentsConstraintsProvider>(context, listen: false);
+
+    bool isLengthGreaterThanWidth = MediaQuery.of(context).size.height > MediaQuery.of(context).size.width;
 
     List<Widget> contents = [];
     for (int i = 0; i < tableOfContents.length; i++) {
       contents.addAll([
         Container(
-          padding:  EdgeInsets.symmetric(horizontal: width/24.725),
+          padding: EdgeInsets.symmetric(horizontal: isLengthGreaterThanWidth ? 0 : 80),
           child: TextButton(
             style: ButtonStyle(
               shape: WidgetStateProperty.all<RoundedRectangleBorder>(
@@ -38,21 +37,21 @@ class TableOfContents extends StatelessWidget {
                 curve: Curves.easeInOut,
                 alignment: 0.05,
               );
-
+              if (isLengthGreaterThanWidth) context.pop();
             },
             child: Row(
               children: [
                 Icon(
                   Icons.circle,
                   color: fontColor,
-                  size: width/123.625,
+                  size: 16,
                 ),
-                 SizedBox(
-                  width: width/98.9-5,
+                const SizedBox(
+                  width: 20 - 5,
                 ),
                 Text(
                   tableOfContents[i].title,
-                  style: TextStyle(fontSize: width/76.07692 , color: fontColor),
+                  style: TextStyle(fontSize: isLengthGreaterThanWidth ? 18 : 26, color: fontColor),
                 ),
               ],
             ),
@@ -60,8 +59,8 @@ class TableOfContents extends StatelessWidget {
         ),
         Divider(
           height: 10,
-          indent: width/24.725,
-          endIndent: width/24.725,
+          indent: isLengthGreaterThanWidth ? 10 : 80,
+          endIndent: isLengthGreaterThanWidth ? 10 : 80,
           color: fontColor,
         )
       ]);
@@ -94,6 +93,7 @@ class TableOfContents extends StatelessWidget {
     }
 
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: contents,
     );
   }
